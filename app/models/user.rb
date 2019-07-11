7f2b8251381d7f81bb5f7b6aa8951eb19bef0c02
tezebuike <tracy.ezebuike@andela.com>
2019-07-11 09:49:57 +0100

    Add validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :role, presence: true
+  validates :username,  presence: true, length: { maximum: 25 },
+                    uniqueness: { case_sensitive: false }, allow_blank: false
 end
