ef9c295b9dd64db60e771a58a219fa92a19018b2
tezebuike <tracy.ezebuike@andela.com>
2019-07-11 10:10:40 +0100

    Add helper methods to manage user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      log_in user
+      redirect_to root_url, notice: "Logged in!"
+    else
+      flash.now[:alert] = "Username or password is invalid"
+      render "new"
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_url, notice: "Logged out!"
   end
 end
