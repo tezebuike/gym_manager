62e329b276ce6eb7d87022a4538f4041ace52409
tezebuike <tracy.ezebuike@andela.com>
2019-07-14 23:08:36 +0100

    Allow user to be remembered

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_username(params[:username])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_username(params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
       sign_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to root_url, notice: "You have been signed in!"
     else
       flash.now[:alert] = "Username or password is invalid"
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    sign_out
+    sign_out if signed_in?
     redirect_to root_url, notice: "You have been signed out!"
   end
 end
