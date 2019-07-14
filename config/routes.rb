556242215f148a11b874ab2ecc4aa7cf6033602f
tezebuike <tracy.ezebuike@andela.com>
2019-07-14 21:13:41 +0100

    Add sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   
   root 'sessions#new'
 
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  get    '/signin',   to: 'sessions#new'
+  post   '/signin',   to: 'sessions#create'
+  delete '/signout',  to: 'sessions#destroy'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
