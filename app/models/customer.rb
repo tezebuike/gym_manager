4fb77c62c410ff3e5547e22f64b7d231cf055f56
tezebuike <tracy.ezebuike@andela.com>
2019-07-28 17:21:28 +0100

    Add full name to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   has_many :subscriptions
   has_many :plans, :through => :subscriptions
   has_many :attendances
+
+  def full_name
+    return "#{first_name} #{middle_name} #{last_name}"
+  end
 end
