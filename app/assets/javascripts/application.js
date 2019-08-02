4c7b03442a73eb49d0b30a3ff033b1770cab830b
tezebuike <tracy.ezebuike@andela.com>
2019-08-02 16:37:20 +0100

    Add functionality to upload pictures

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,4 +29,6 @@ $(document).ready(function() {
   }, 2000);
 
   $("#dataTable").dataTable();
+
+  bsCustomFileInput.init();
 });
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -22,7 +22,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process scale: [300, 300]
+  # process resize_to_fit: [300, 300]
   #
   # def scale(width, height)
   #   # do something
@@ -30,10 +30,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [230, 180]
-  end
-
-  version :user_profile do
     process resize_to_fit: [60, 60]
   end
 
