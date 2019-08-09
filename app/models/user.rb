8ade05703795c8d8c6599d351513bed4c50eb5bb
tezebuike <tracy.ezebuike@andela.com>
2019-08-09 18:39:17 +0100

    Minor fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,13 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
